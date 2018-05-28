14b5376d569207ce0efe777ff9379f475f2be962
Ronny Standtke <ronny.standtke@gmx.net>
2018-05-28 12:20:41 +0200

    automatically reboot after timeout

--- src/ch/lernstick/vanishedroothandler/view/javafx/MainSceneController.java
+++ src/ch/lernstick/vanishedroothandler/view/javafx/MainSceneController.java
@@ -2,10 +2,15 @@ package ch.lernstick.vanishedroothandler.view.javafx;
 
 import ch.lernstick.vanishedroothandler.VanishedRootHandler;
 import java.net.URL;
+import java.text.MessageFormat;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+import javafx.beans.binding.Bindings;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * the controller for the main scene
@@ -14,15 +19,48 @@ import javafx.scene.control.Button;
  */
 public class MainSceneController implements Initializable {
 
+    private static final ResourceBundle STRINGS = ResourceBundle.getBundle(
+            "ch/lernstick/vanishedroothandler/Strings");
+    private static final int TIMEOUT = 30;
+
     @FXML
     private Button rebootButton;
 
     @FXML
     private Button powerOffButton;
 
+    @FXML
+    private Label timeoutLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         rebootButton.setOnAction(a -> VanishedRootHandler.reboot());
         powerOffButton.setOnAction(a -> VanishedRootHandler.powerOff());
+
+        Task<Integer> timeoutTask = new Task<Integer>() {
+            @Override
+            protected Integer call() throws Exception {
+                for (int i = 0; i < TIMEOUT; i++) {
+                    updateProgress(i, TIMEOUT);
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                VanishedRootHandler.reboot();
+                return null;
+            }
+        };
+
+        timeoutLabel.textProperty().bind(
+                Bindings.createStringBinding(() -> {
+                    int remainingSeconds = TIMEOUT
+                            - (int) (TIMEOUT * timeoutTask.getProgress());
+                    String timeUnitString = STRINGS.getString(
+                            remainingSeconds == 1 ? "Second" : "Seconds");
+                    return MessageFormat.format(
+                            STRINGS.getString("Auto_Reboot_Label"),
+                            remainingSeconds + " " + timeUnitString);
+                }, timeoutTask.progressProperty())
+        );
+
+        new Thread(timeoutTask).start();
     }
 }
